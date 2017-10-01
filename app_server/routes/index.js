28b0de12fae2d2da336da9c6369440dc9dd60901
den330 <yaxinyuan0910@gmail.com>
2017-10-01 10:52:24 -0400

    view added

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,7 @@ var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
 
-router.get('/', ctrlLocations.homeList);
+router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 

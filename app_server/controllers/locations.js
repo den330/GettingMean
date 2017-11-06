3fa0937cc473e225a9c2b56c97fee6fc464f5827
den330 <yaxinyuan0910@gmail.com>
2017-11-06 13:59:42 -0500

    pg 216

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,14 +10,24 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 var renderHomepage = function(req, res, responseBody){
-	    res.render('locations-list', {
-        title: 'Loc8r - find a place to work with wifi',
-        pageHeader: {
-            title: 'Loc8r',
-            strapline: 'Find places to work with wifi near you!'
-        },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-		locations: responseBody
+	var message;
+	if (!(responseBody instanceof Array)){
+		message = "API lookup error"
+		responseBody = [];
+	}else{
+		if(!responseBody.length){
+			message = "No places found nearby";
+		}
+	}
+	res.render('locations-list', {
+	title: 'Loc8r - find a place to work with wifi',
+	pageHeader: {
+		title: 'Loc8r',
+		strapline: 'Find places to work with wifi near you!'
+	},
+	sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+	locations: responseBody,
+	message: message
     });
 };
 	
@@ -32,7 +42,7 @@ module.exports.homelist = function(req, res) {
 		qs: {
 			lng: -0.7992599,
 			lat: 51.378091,
-			maxDistance: 20
+			maxDistance: 2000
 		}
 	};
 	request(requestOptions, function(err, response, body){

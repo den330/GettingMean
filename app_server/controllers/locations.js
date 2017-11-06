68d2b493564f2dd4074d1933d3e8b6c863a7a1f8
den330 <yaxinyuan0910@gmail.com>
2017-11-06 13:42:30 -0500

    pg 209

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,33 +1,43 @@
 /* GET 'home' page */
 
-module.exports.homelist = function(req, res) {
-    res.render('locations-list', {
+var request = require('request');
+
+var apiOptions = {
+	server: "http://localhost:3000"
+};
+if(process.env.NODE_ENV === 'production'){
+	apiOptions.server = "https://frozen-citadel-15362.herokuapp.com/";
+}
+
+var renderHomepage = function(req, res, responseBody){
+	    res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-        }, {
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-        }, {
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-        }]
+		locations: responseBody
     });
+};
+	
+
+module.exports.homelist = function(req, res) {
+	var requestOptions, path;
+	path = '/api/locations';
+	requestOptions = {
+		url: apiOptions.server + path,
+		mathod: "GET",
+		json: {},
+		qs: {
+			lng: -0.7992599,
+			lat: 51.378091,
+			maxDistance: 20
+		}
+	};
+	request(requestOptions, function(err, response, body){
+		renderHomepage(req, res, body);
+	});
 };
 
 /* GET 'Location info' page */

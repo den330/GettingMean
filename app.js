f209fc641d7a9ffaf550bf51a01bdc898735e8ea
den330 <yaxinyuan0910@gmail.com>
2017-12-10 08:58:13 -0500

    minor change

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var users = require('./app_server/routes/users');
 
 var app = express();
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
